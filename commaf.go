a6fa2fcc838db78a342d492b9ec156a22c608857
Adnan Ademovic <adnanademovic100@gmail.com>
2017-08-01 14:31:08 +0200

    Allow big.Float rounding and custom runes

--- commaf.go
+++ commaf.go
@@ -11,29 +11,35 @@ import (
 // BigCommaf produces a string form of the given big.Float in base 10
 // with commas after every three orders of magnitude.
 func BigCommaf(v *big.Float) string {
+	return CustomBigCommaf(v, -1, ',', '.')
+}
+
+// CustomBigCommaf produces a string form of the given big.Float in base 10
+// with given rune after every three orders of magnitude,
+// as well as the provided rune for the decimal point.
+// Provide non-negative number of decimals for a fixed format, or -1 otherwise.
+func CustomBigCommaf(v *big.Float, decimals int, kilo, point rune) string {
 	buf := &bytes.Buffer{}
 	if v.Sign() < 0 {
 		buf.Write([]byte{'-'})
 		v.Abs(v)
 	}
 
-	comma := []byte{','}
-
-	parts := strings.Split(v.Text('f', -1), ".")
+	parts := strings.Split(v.Text('f', decimals), ".")
 	pos := 0
 	if len(parts[0])%3 != 0 {
 		pos += len(parts[0]) % 3
 		buf.WriteString(parts[0][:pos])
-		buf.Write(comma)
+		buf.WriteRune(kilo)
 	}
 	for ; pos < len(parts[0]); pos += 3 {
 		buf.WriteString(parts[0][pos : pos+3])
-		buf.Write(comma)
+		buf.WriteRune(kilo)
 	}
 	buf.Truncate(buf.Len() - 1)
 
 	if len(parts) > 1 {
-		buf.Write([]byte{'.'})
+		buf.WriteRune(point)
 		buf.WriteString(parts[1])
 	}
 	return buf.String()
--- commaf_test.go
+++ commaf_test.go
@@ -42,3 +42,39 @@ func TestBigCommafs(t *testing.T) {
 		{"-10", BigCommaf(big.NewFloat(-10)), "-10"},
 	}.validate(t)
 }
+
+func TestCustomBigCommafs(t *testing.T) {
+	testList{
+		{"0", CustomBigCommaf(big.NewFloat(0), -1, '.', ','), "0"},
+		{"10.11", CustomBigCommaf(big.NewFloat(10.11), -1, '.', ','), "10,11"},
+		{"100", CustomBigCommaf(big.NewFloat(100), -1, '.', ','), "100"},
+		{"1,000", CustomBigCommaf(big.NewFloat(1000), -1, '.', ','), "1.000"},
+		{"10,000", CustomBigCommaf(big.NewFloat(10000), -1, '.', ','), "10.000"},
+		{"100,000", CustomBigCommaf(big.NewFloat(100000), -1, '.', ','), "100.000"},
+		{"834,142.32", CustomBigCommaf(big.NewFloat(834142.32), -1, '.', ' '), "834.142 32"},
+		{"10,000,000", CustomBigCommaf(big.NewFloat(10000000), -1, '^', ','), "10^000^000"},
+		{"10,100,000", CustomBigCommaf(big.NewFloat(10100000), -1, '%', ','), "10%100%000"},
+		{"10,010,000", CustomBigCommaf(big.NewFloat(10010000), -1, '+', ','), "10+010+000"},
+		{"10,001,000", CustomBigCommaf(big.NewFloat(10001000), -1, '/', ','), "10/001/000"},
+		{"123,456,789", CustomBigCommaf(big.NewFloat(123456789), -1, '.', ','), "123.456.789"},
+		{"maxf64", CustomBigCommaf(big.NewFloat(math.MaxFloat64), -1, ' ', ','), "179 769 313 486 231 570 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000"},
+		{"minf64", CustomBigCommaf(big.NewFloat(math.SmallestNonzeroFloat64), -1, '.', '|'), "0|000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004940656458412465"},
+		{"minf64 fixed", CustomBigCommaf(big.NewFloat(math.SmallestNonzeroFloat64), 4, '.', '|'), "0|0000"},
+		{"-123,456,789", CustomBigCommaf(big.NewFloat(-123456789), -1, ':', ','), "-123:456:789"},
+		{"-10,100,000", CustomBigCommaf(big.NewFloat(-10100000), -1, '|', ','), "-10|100|000"},
+		{"-10,010,000", CustomBigCommaf(big.NewFloat(-10010000), -1, '*', ','), "-10*010*000"},
+		{"-10,001,000", CustomBigCommaf(big.NewFloat(-10001000), -1, '?', ','), "-10?001?000"},
+		{"-10,000,000", CustomBigCommaf(big.NewFloat(-10000000), -1, '!', ','), "-10!000!000"},
+		{"-100,000", CustomBigCommaf(big.NewFloat(-100000), -1, '.', ','), "-100.000"},
+		{"-10,000", CustomBigCommaf(big.NewFloat(-10000), -1, '.', ','), "-10.000"},
+		{"-1,000", CustomBigCommaf(big.NewFloat(-1000), -1, '.', ','), "-1.000"},
+		{"-100.11", CustomBigCommaf(big.NewFloat(-100.11), -1, '.', ','), "-100,11"},
+		{"-10", CustomBigCommaf(big.NewFloat(-10), -1, '.', ','), "-10"},
+		{"834,142.3211 fixed to 2", CustomBigCommaf(big.NewFloat(834142.3211), 2, '.', ' '), "834.142 32"},
+		{"834,142.3211 fixed to 0", CustomBigCommaf(big.NewFloat(834142.3211), 0, '.', ' '), "834.142"},
+		{"834,142.3250", CustomBigCommaf(big.NewFloat(834142.3250), -1, '.', ' '), "834.142 325"},
+		{"834,142.3250 fixed to 2 rounds down", CustomBigCommaf(big.NewFloat(834142.3250), 2, '.', ' '), "834.142 32"},
+		{"834,142.3251", CustomBigCommaf(big.NewFloat(834142.3251), -1, '.', ' '), "834.142 3251"},
+		{"834,142.3251 fixed to 2 rounds up", CustomBigCommaf(big.NewFloat(834142.3251), 2, '.', ' '), "834.142 33"},
+	}.validate(t)
+}
